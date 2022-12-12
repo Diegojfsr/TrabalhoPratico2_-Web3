5d31e50a02b0347f92a436b91026218d936f30fc
Diego Jefferson <diegojfsr@gmail.com>
2022-12-12 10:07:27 -0300

    Lista Tarefas

---
+++ lista-tarefas/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="main">
+    <h3>Nome Aluno</h3>
+    <InputTarefas @novaTarefa="addTarefa"></InputTarefas>
+
+
+
+    <ListaTarefas :listaItem="tarefas"></ListaTarefas>
+  </div>
+</template>
+
+<script>
+import InputTarefas from './components/InputTarefas.vue'
+import ListaTarefas from './components/ListaTarefas.vue';
+
+export default {
+  name: 'App',
+  components: {
+    InputTarefas,
+    ListaTarefas
+  },
+  data(){
+    return{
+      tarefas: []
+    }
+  },
+  methods:{
+    addTarefa(tarefa){
+      this.tarefas.push(tarefa);
+      console.log(this.tarefas);
+    }
+  }
+}
+</script>
+
+<style>
+#main{
+  width: 75%;
+  margin: auto;
+}
+</style>
